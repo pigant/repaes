0ebc1a896dec87f141434b0262780c70737803ad
Ignacio Ruz <ignacio.ruz04@inacapmail.cl>
2016-12-14 22:30:55 -0300

    Cambios de enzo

---
+++ App_Code/BL/UserBL.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel;
+
+/// <summary>
+/// Descripción breve de UserBL
+/// </summary>
+///
+  [DataObject]
+  public class UserBL
+  {
+    private DatabaseEntities Bd = new DatabaseEntities();
+
+    public bool ValidateUser(string username, string password)
+    {
+        List<User> list = (from u in Bd.User
+                           where u.Username == username && u.Password == password
+                           select u).ToList();
+
+        return list.Count > 0;
+    }
+
+    public User getUser(string username)
+    {
+      return (from u in Bd.User where u.Username == username select u).FirstOrDefault;
+    }
+
+
+    
+  }
---
+++ App_Code/RPUser.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+
+/// <summary>
+/// Descripción breve de RPUser
+/// </summary>
+public class RPUser : RoleProvider
+{
+    public override string ApplicationName
+    {
+        get
+        {
+            throw new NotImplementedException();
+        }
+
+        set
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public override void AddUsersToRoles(string[] usernames, string[] roleNames)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void CreateRole(string roleName)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override bool DeleteRole(string roleName, bool throwOnPopulatedRole)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override string[] FindUsersInRole(string roleName, string usernameToMatch)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override string[] GetAllRoles()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override string[] GetRolesForUser(string username)
+    {
+        UserBL ubl = new UserBL();
+        var user = ubl.getUser(username);
+        string[] rols = new string[1];
+        rols[0] =  Convert.ToString(user.NumberGroup);
+        return rols;
+    }
+
+    public override string[] GetUsersInRole(string roleName)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override bool IsUserInRole(string username, string roleName)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void RemoveUsersFromRoles(string[] usernames, string[] roleNames)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override bool RoleExists(string roleName)
+    {
+        throw new NotImplementedException();
+    }
+}

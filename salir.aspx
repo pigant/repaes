5b63b16159cad9c57bd341a3316b1af5674dfcb7
Ignacio Ruz <ignacio.ruz04@inacapmail.cl>
2016-12-15 00:20:34 -0300

    Salir y vinculos

---
+++ salir.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="salir.aspx.cs" Inherits="salir" %>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head><title></title></head>
+<body></body>
+</html>
---
+++ salir.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class salir : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        FormsAuthentication.SignOut();
+
+        // Drop all the information held in the session
+        Session.Clear();
+        Session.Abandon();
+
+        // clear authentication cookie
+        HttpCookie cookie1 = new HttpCookie(FormsAuthentication.FormsCookieName, "");
+        cookie1.Expires = DateTime.Now.AddYears(-1);
+        Response.Cookies.Add(cookie1);
+
+        // clear session cookie
+        HttpCookie cookie2 = new HttpCookie("ASP.NET_SessionId", "");
+        cookie2.Expires = DateTime.Now.AddYears(-1);
+        Response.Cookies.Add(cookie2);
+
+        // Redirect the user to the login page
+        Response.Redirect("menu.aspx");
+    }
+}

03c338e3ee19f84ef3cb36fd30c08c27523b1a50
rodriguitto <rodrigo_xion@hotmail.com>
2016-12-15 17:57:00 -0400

    grilla y la conche.....

--- resumen.aspx.cs
+++ resumen.aspx.cs
@@ -5,10 +5,17 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+
 public partial class resumen : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
 
+    }
+    protected void CalSalida_SelectionChanged(object sender, EventArgs e)
+    {
+        metodos m = new metodos();
+        m.GetRecordsFecha(CalSalida.SelectedDate);
+        GridResumen.DataBind();
     }
 }
